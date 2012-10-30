a64cb64de6c65e0fad4410a1cedf277671e929bf
kenju254 <kenmbugua@gmail.com>
2012-10-30 17:35:50 +0300

    This is exercise 10 cant remember what this was talking about but just check it out.....

---
+++ LearnPythonTheHardWay/ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI' tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+ I' will do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip
+\t* Grass
+"""
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
